b86f830ada91e7422f470d7b14ada356c7149f7b
Lucas Birkert (Laptop) <kekontheworld@gmail.com>
2023-08-14 09:59:30 +0200

    Update benchmark

--- crates/kelocam-core/benches/ray_intersection.rs
+++ crates/kelocam-core/benches/ray_intersection.rs
@@ -7,67 +7,61 @@ use test::Bencher;
 
 #[bench]
 pub fn ray_triangle_intersection(bencher: &mut Bencher) {
-    bencher.iter(|| {
-        let triangle: Triangle;
-        {
-            let a = Vector3::new_random();
-            let b = Vector3::new_random();
-            let c = Vector3::new_random();
-            let normal = UnitVector3::new_normalize((a - b).cross(&(a - c)));
-            triangle = Triangle::new(a, b, c, normal);
-        }
+    let triangle: Triangle;
+    {
+        let a = Vector3::new_random();
+        let b = Vector3::new_random();
+        let c = Vector3::new_random();
+        let normal = UnitVector3::new_normalize((a - b).cross(&(a - c)));
+        triangle = Triangle::new(a, b, c, normal);
+    }
 
-        let ray: Ray;
-        {
-            let origin = Vector3::new_random();
-            let normal = UnitVector3::new_normalize(Vector3::new_random());
-            ray = Ray::new(origin, normal);
-        }
+    let ray: Ray;
+    {
+        let origin = Vector3::new_random();
+        let normal = UnitVector3::new_normalize(Vector3::new_random());
+        ray = Ray::new(origin, normal);
+    }
 
-        ray.intersect(&triangle)
-    })
+    bencher.iter(|| test::black_box(&ray).intersect(test::black_box(&triangle)));
 }
 
 #[bench]
 pub fn ray_plane_intersection(bencher: &mut Bencher) {
-    bencher.iter(|| {
-        let plane: Plane;
-        {
-            let origin = Vector3::new_random();
-            let normal = UnitVector3::new_normalize(Vector3::new_random());
-            plane = Plane::new(origin, normal);
-        }
+    let plane: Plane;
+    {
+        let origin = Vector3::new_random();
+        let normal = UnitVector3::new_normalize(Vector3::new_random());
+        plane = Plane::new(origin, normal);
+    }
 
-        let ray: Ray;
-        {
-            let origin = Vector3::new_random();
-            let normal = UnitVector3::new_normalize(Vector3::new_random());
-            ray = Ray::new(origin, normal);
-        }
+    let ray: Ray;
+    {
+        let origin = Vector3::new_random();
+        let normal = UnitVector3::new_normalize(Vector3::new_random());
+        ray = Ray::new(origin, normal);
+    }
 
-        ray.intersect(&plane)
-    })
+    bencher.iter(|| test::black_box(&ray).intersect(test::black_box(&plane)))
 }
 
 #[bench]
 pub fn ray_square_intersection(bencher: &mut Bencher) {
-    bencher.iter(|| {
-        let square: Square;
-        {
-            let a = Vector3::new_random();
-            let ab = Vector3::new_random();
-            let ac = Vector3::new_random();
-            let normal = UnitVector3::new_normalize(ab.cross(&ac));
-            square = Square::new(a, ab, ac, normal);
-        }
+    let square: Square;
+    {
+        let a = Vector3::new_random();
+        let ab = Vector3::new_random();
+        let ac = Vector3::new_random();
+        let normal = UnitVector3::new_normalize(ab.cross(&ac));
+        square = Square::new(a, ab, ac, normal);
+    }
 
-        let ray: Ray;
-        {
-            let origin = Vector3::new_random();
-            let normal = UnitVector3::new_normalize(Vector3::new_random());
-            ray = Ray::new(origin, normal);
-        }
+    let ray: Ray;
+    {
+        let origin = Vector3::new_random();
+        let normal = UnitVector3::new_normalize(Vector3::new_random());
+        ray = Ray::new(origin, normal);
+    }
 
-        ray.intersect(&square)
-    })
+    bencher.iter(|| test::black_box(&ray).intersect(test::black_box(&square)))
 }
